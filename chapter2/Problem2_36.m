dac16cced1b158651cbcd63843a1b57525ed1740
yueyangming <yueyangming1024@sina.com>
2016-01-06 19:10:45 +0800

    Correction of 2-36

--- chapter2/Problem2_36.m
+++ chapter2/Problem2_36.m
@@ -1,6 +1,7 @@
 % Problem 2.36
 
-clear all; close all;
+clear all; 
+close all;
 
 x = randn(1,500);
 h = [1/3 1/3 1/3];
@@ -8,11 +9,15 @@ y = conv(h,x);
 
 figure;
 subplot(2,1,1);
-plot(axcor(y));
-axis([500-20 500+20 -0.2 1]); % Limit x axis to +-20 lags
+% plot(axcor(y));
+[rxx,lags] = axcor(y);
+plot(lags,rxx);
+axis([-20 20 -0.2 1]); % Limit x axis to +-20 lags
 title ('Autocorrelation of filtered one')
 
 subplot(2,1,2);
-plot(axcor(x));
-axis([500-20 500+20 -0.2 1]); % Limit x axis to +-20 lags
+[rxx,lags] = axcor(x);
+% plot(axcor(x));
+plot(lags,rxx);
+axis([-20 20 -0.2 1]); % Limit x axis to +-20 lags
 title('Origin autocorrelation');
