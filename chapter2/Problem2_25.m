d573917314ab9247f9f6c6ea4b203f89369c124f
yueyangming <yueyangming1024@sina.com>
2016-01-07 14:58:48 +0800

    Correction of 2.25

--- chapter2/Problem2_25.m
+++ chapter2/Problem2_25.m
@@ -3,8 +3,10 @@
 % First, x = cos ( 2 * pi * 1 * t) y is a square function
 clear all; close all;
 
+N = 200;
+
 TT = 2;
-Ts = 0.01;
+Ts = TT / N;
 t = 0 : Ts : TT - Ts;
 
 x = cos(2 * pi * 1 * t);
@@ -12,5 +14,9 @@ y = ones(1,TT / Ts);
 y(51:100) = -1;
 y(151 : 200) = -1;
 
-Corr = sum(x.*y);
+plot(t,y,'b');
+hold on;
+plot(t,x,'r');
+
+Corr = sum(x.*y)/N;
 disp(['Corr = ',num2str(Corr)]);
